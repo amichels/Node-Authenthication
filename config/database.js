2d861b9d091a98eeda6169d83a6df578fe121f97
amichels <andy.j.michels@gmail.com>
2014-06-09 22:09:50 -0500

    database connection

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
 
-	'url' : 'mongodb://localhost/todo-app' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : 'mongodb://localhost/cellar' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
